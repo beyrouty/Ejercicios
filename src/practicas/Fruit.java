b0f32cf8e6f92814f79106211bb5e09d54d018e8
Fabiola Cristina Beyrouty Zammar <fab_beyrouty@hotmail.com>
2012-09-27 22:59:33 -0600

    Fruit

---
+++ src/practicas/Fruit.java
@@ -0,0 +1,19 @@
+
+package practicas;
+
+public class Fruit {
+
+public static void main (String[] args) {
+  
+    int numOranges = 6;
+    int numApples = 10;
+    int numFruit = 0;
+        
+  numFruit=numOranges+numApples;
+        
+ System.out.println("EL total de frutas es: " + numFruit);
+  
+   }
+}
+    
+
